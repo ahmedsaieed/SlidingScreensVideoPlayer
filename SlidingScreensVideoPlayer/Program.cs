7665aeb30a869d65eb338b2a18ab12bcc146f092
Ahmed Saieed <a.saieed@gmail.com>
2014-12-11 13:23:17 +0400

    Initial Commit
    Pseudo-code and comments. No code yet.

---
+++ SlidingScreensVideoPlayer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlidingScreensVideoPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start VLC
+
+            // Find playlist.xml file
+
+            // Check last modification date
+
+            // If modification date of playlist.xml.bak != modification date of playlist.xml
+
+                // Run homing (and wait until it's over)
+
+                // Copy playlist.xml to playlist.xml.bak
+
+                // Parse playlist.xml.bak
+
+            // Loop over playlist items
+                // Play video
+
+                // Run corrosponding motion executable
+
+                // Wait until end of video is reached (either with endofplaylistevent or by waiting the video length)
+
+        }
+
+
+
+    }
+}
